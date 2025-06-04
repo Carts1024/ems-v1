b927752751c3596692a40ba1ee915094ca6bc3d6
Carl Bergado <carlaldreydbergado@iskolarngbayan.pup.edu.ph>
2025-06-05 04:22:58 +0800

    Latest update for removing cookies

--- bus-employee-management-system/client/app/utils/logout.ts
+++ bus-employee-management-system/client/app/utils/logout.ts
@@ -1,7 +1,13 @@
 export async function logout() {
-    const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
-    await fetch(`${API_BASE_URL}/auth/logout`, {
+    // const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+    // await fetch(`${API_BASE_URL}/auth/logout`, {
+    //     method: 'POST',
+    //     credentials: 'include', // This makes sure cookies are sent and cleared
+    // });
+    await fetch('/api/auth/login', {
         method: 'POST',
-        credentials: 'include', // This makes sure cookies are sent and cleared
+        headers: { 'Content-Type': 'application/json' },
+        credentials: 'include',
     });
+
 }

f821d7918301adfa9966b2e9b195a05f3ab63664
Carl Bergado <carlaldreydbergado@iskolarngbayan.pup.edu.ph>
2025-06-06 20:32:23 +0800

    Update auth.controller.ts
    added domain in cookie

--- bus-employee-management-system/backend/employee-management-backend/apps/auth-ms/src/auth/auth.controller.ts
+++ bus-employee-management-system/backend/employee-management-backend/apps/auth-ms/src/auth/auth.controller.ts
@@ -46,6 +46,7 @@ export class AuthController {
       httpOnly: true,
       secure: true, // Always true in production (HTTPS)
       sameSite: 'none', // Required for cross-site cookies
+      domain: '.agilabuscorp.me',
       path: '/',
       maxAge: 3600 * 1000,
     });

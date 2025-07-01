597521a042ef2d461e07ba0f98ae6c9ee74c5d2f
akiszn <138987400+akiszn@users.noreply.github.com>
2025-07-01 17:48:28 +0800

    Updated Facial Recognition Modal

--- bus-employee-management-system/client/components/modal/attendance/FacialRecognitionModal.tsx
+++ bus-employee-management-system/client/components/modal/attendance/FacialRecognitionModal.tsx
@@ -1,20 +1,18 @@
 'use client';
 
 import React from 'react';
-import styles from './AttendanceModal.module.css';
-import { useFacialRecognitionModalLogic } from './FacialRecognitionModalLogic'; 
+import styles from './FacialRecognitionModal.module.css';
 import { Attendance } from '@/app/homepage/attendance/daily-report/dailyReportLogic'; 
 
 interface FacialRecognitionModalProps {
   onClose: () => void;
-  onScanSuccess: (attendance: Attendance) => void;
+  onScanSuccess: (attendance: Attendance) => void; 
 }
 
 const FacialRecognitionModal: React.FC<FacialRecognitionModalProps> = ({
   onClose,
   onScanSuccess,
 }) => {
-  const { handleSimulateScan } = useFacialRecognitionModalLogic(onScanSuccess);
 
   return (
     <div className={styles.modalOverlay}>
@@ -22,26 +20,15 @@ const FacialRecognitionModal: React.FC<FacialRecognitionModalProps> = ({
         <button className={styles.closeButton} onClick={onClose}>
           <i className="ri-close-line" />
         </button>
-
-        <div className={`${styles.sectionHeader} ${styles.centeredHeader}`}>
-          <h1 className={styles.heading}>Facial Recognition Attendance</h1>
-        </div>
-
-        <div className={styles.cameraPlaceholderContainer}> 
-           <i className={`ri-camera-fill ${styles.cameraIcon}`} />
+        <div className={styles.cameraPlaceholderContainer}>
+          <i className={`ri-camera-fill ${styles.cameraIcon}`} />
           <p>Camera Placeholder</p>
         </div>
 
-        <p className={styles.cameraInstructionText}> 
-          Please look at the camera to record your attendance.
+        <p className={styles.cameraInstructionText}>
+          Please look at the camera to record your attendance. <br/>
+          (Mangyaring tumingin sa kamera upang maitala ang iyong pagdalo.)
         </p>
-
-        <div className={styles.buttonGroup}>
-          <button onClick={onClose} className={styles.cancelButton}>Close</button>
-          <button onClick={handleSimulateScan} className={styles.submitButton}>
-            Record
-          </button>
-        </div>
       </div>
     </div>
   );
